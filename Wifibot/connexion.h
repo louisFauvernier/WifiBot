3ebdb4df3c10cf2b52f949f6a40062afd0662112
Louis Fauvernier <louis.fauvernier@hotmail.fr>
2015-04-03 08:16:31 +0200

    Update Alpha 1.01

--- Wifibot/connexion.h
+++ Wifibot/connexion.h
@@ -2,16 +2,34 @@
 #define CONNEXION_H
 
 #include <QObject>
+#include <QTcpSocket>
+#include <QTimer>
 
 class connexion : public QObject
 {
     Q_OBJECT
 public:
-    explicit connexion(QObject *parent = 0);
+    connexion(QObject *parent = 0);
+    QByteArray buf;
+    QString adresse;
+    QString port;
+    bool connecte;
+    bool foreward;
 
-signals:
+    QTcpSocket tcp;
+    QTimer *timer;
+    void Connexion();
+    void Deconnexion();
+    void GenMessage();
+    void sendMessage();
+    void recvMessage();
+    quint16 vitesse;
+    quint16 battery;
+    quint16 crc16(QByteArray byteArray, int pos);
 
 public slots:
+         void tick();
+signals:
 
 };
 
